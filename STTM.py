5953581df3d06a2bfab2c4e80982b65a6e0e2e7c
Harsh <harsh.blogspot@gmail.com>
2023-01-16 18:22:05 +0530

    Added a ErrorLoging System

--- STTM.py
+++ STTM.py
@@ -1,3 +1,11 @@
+import datetime
+
+
+config ={
+        'LOG':'.\\LOG\\'
+    }
+
+
 class Send():
     def __init__(self):
         pass
@@ -9,8 +17,15 @@ class FileShare():
     pass
 
 class ErrorManager():
+    
     def __init__(self):pass
 
     @staticmethod
-    def ReportLog(ErrorMsg):
-        pass
+    def ReportLog(ErrorMsg,comment):
+        NameOfLogFile = ''.join(list({
+            'NAME':datetime.datetime.now().strftime("%w%d%m%Y%H%M%S%f"),
+            'EXT':'.logfile'}))
+        
+        with open(config['LOG']+NameOfLogFile,'a+')as LogFile:LogFile.write(str(ErrorMsg),comment)
+        input('There was an error during the {}, the error has been logged... error id is {}\nHit Entere to continue'.format(comment,ErrorMsg))    
+        exit()
