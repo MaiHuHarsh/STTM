38fc6631cf10dab37c2f3db9d33ff01aaf2f626f
Harsh <harsh.blogspot@gmail.com>
2023-01-16 18:04:06 +0530

    initial Setups

---
+++ STTM.py
@@ -0,0 +1,16 @@
+class Send():
+    def __init__(self):
+        pass
+
+class Recv():
+    pass
+
+class FileShare():
+    pass
+
+class ErrorManager():
+    def __init__(self):pass
+
+    @staticmethod
+    def ReportLog(ErrorMsg):
+        pass
