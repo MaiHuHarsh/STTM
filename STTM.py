a8a9e0a97e56a27fd8ce35f037fff24e9b13b5af
Harsh <harsh.blogspot@gmail.com>
2023-01-16 18:39:08 +0530

    Build SEND class ( Did't beta test it ) [:]

--- STTM.py
+++ STTM.py
@@ -1,5 +1,5 @@
 import datetime
-
+import socket, os, base64, json
 
 config ={
         'LOG':'.\\LOG\\'
@@ -7,13 +7,38 @@ config ={
 
 
 class Send():
-    def __init__(self):
-        pass
+    
+    def __init__(self,port):
+        if type(port)!=int:
+            try:port=int(port)
+            except ValueError: ErrorManager.ReportError('Invalid Port input', 'The Port number is suppoed to be an Integer');return
+        self.ThisPC = socket.socket()
+        self.Configration = (socket.gethostbyaddr(socket.gethostname()),port)
+        self.ThisPC.bind(self.Configration)
+    
+    def StartAcceptingRequests(self):
+        self.ThisPC.listen()
+        self.Receiver, self.ReceiverAddr = self.ThisPC.accept()
+    
+    def BuildPacakgeToSend(self,file):
+        if not os.path.isfile(file): ErrorManager.ReportError('Invalid File','The File Selected is invalid')
+        FILE ={}
+        with open(file,'rb')as TragetFileData:FILE['DATA'] = base64.b64encode(TragetFileData.read()).decode()
+        FILE['FILENAME']=str(file).split('\\')[::-1][0]
+        Package = json.dumps(FILE).encode()
+        CONFIG = {'SIZE':len(Package)}
+        return Package, CONFIG
+    
+    def SendFile(self,Package,CONFIG):
+        try:
+            self.Receiver.sendall(CONFIG)
+            self.Receiver.sendAll(Package)
+        except Exception as Error:
+            ErrorManager.ReportLog(Error,"Shareing the CONFIG and Package")
+
+
 
 class Recv():
-    pass
-
-class FileShare():
     pass
 
 class ErrorManager():
@@ -29,3 +54,7 @@ class ErrorManager():
         with open(config['LOG']+NameOfLogFile,'a+')as LogFile:LogFile.write(str(ErrorMsg),comment)
         input('There was an error during the {}, the error has been logged... error id is {}\nHit Entere to continue'.format(comment,ErrorMsg))    
         exit()
+    
+    @staticmethod
+    def ReportError(ErrorHeader,ErrorMsg):
+        print('[-] ERROR [-] --> {}\n>>{}'.format(ErrorHeader, ErrorMsg))
